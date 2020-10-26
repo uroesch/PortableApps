a35642eacf17761a6a2114510c8c8206c27284ed
Urs Roesch <github@bun.ch>
2020-10-26 22:57:51 +0000

    cleanup: Better logic and messages
    Summary:
      * Make the switches with a 'Skip'. Default mode
        is now to delete everything unless told otherwise.
      * Display a count of removed packages.
      * Only show header when packages are removed.

--- scripts/cleanup.ps1
+++ scripts/cleanup.ps1
@@ -1,12 +1,27 @@
 #!/usr/bin/env pwsh
 
 <#
+.SYNOPSIS
+  Cleanup PortableApps installers and build depencies installers.
 
+.DESCRIPTION
+  A simple script to cleanup the file in the root of the repository
+  with pattern '*Portable*.paf.exe' and 'PortableApps.com*.paf.exe'.
+  After a few builds it can become quite messy to navigate around
+  the git submodules and the build artifacts.
+
+.PARAMETER SkipApps
+  Skip deletion of the PortableApp installers with pattern
+  '*Portable*.paf.exe'.
+
+.PARAMETER SkipInfra
+  Skip deletion of the PortableApps.com build infractructure
+  installers with pattern 'PortableApps.com*.paf.exe'.
 #>
 
 Param(
-  [Switch] $Apps,
-  [Switch] $Infra
+  [Switch] $SkipApps,
+  [Switch] $SkipInfra
 )
 # -----------------------------------------------------------------------------
 # Globals
@@ -18,24 +33,28 @@ $InstallersDir = Resolve-Path "$PSScriptRoot/.."
 # -----------------------------------------------------------------------------
 Function Remove-Installers() {
   Param(
-    [String] $Filter
+    [String] $Filter,
+    [String] $Message
   )
-  Get-ChildItem -Path $InstallersDir -File -Filter $Filter | ForEach-Object {
+  $Files = Get-ChildItem -Path $InstallersDir -File -Filter $Filter
+  If ($Files.Count -eq 0) { Return }
+  $Message -f $Files.Count, $InstallersDir
+  $Files | ForEach-Object {
     " - Removing File '$_'"
     Remove-Item -Path $_
   }
 }
 
 Function Remove-App-Installers() {
-  If (!($Apps)) { Return }
-  "Removing PortableApps installers from '$InstallersDir'"
-  Remove-Installers -Filter "*Portable_*.paf.exe"
+  If ($SkipApps) { Return }
+  $Message = "Removing {0} PortableApps installers from '{1}'"
+  Remove-Installers -Filter "*Portable_*.paf.exe" -Message $Message
 }
 
 Function Remove-Infra-Installers() {
-  If (!($Infra)) { Return }
-  "Removing build infrastructure installers from '$InstallersDir'"
-  Remove-Installers -Filter "PortableApps.com*.paf.exe"
+  If ($SkipInfra) { Return }
+  $Message = "Removing {0} build infrastructure installers from '{1}'"
+  Remove-Installers -Filter "PortableApps.com*.paf.exe" -Message $Message
 }
 
 # -----------------------------------------------------------------------------
@@ -43,4 +62,3 @@ Function Remove-Infra-Installers() {
 # -----------------------------------------------------------------------------
 Remove-App-Installers
 Remove-Infra-Installers
-
