c75fca283829c284c58dfc97c4bdabe6d5f7eb98
Urs Roesch <github@bun.ch>
2020-11-19 22:14:24 +0100

    build-all: Modify to build with docker

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -10,15 +10,46 @@ set -o pipefail
 # -----------------------------------------------------------------------------
 # Globals
 # -----------------------------------------------------------------------------
+declare -r SCRIPT=${0##*/}
 declare -r BASE_DIR=$(readlink -f $(dirname ${0})/..)
 declare -r SCRIPT_DIR=$(readlink -f $(dirname $0))
 declare -r UPDATE_SCRIPT=Update.ps1
 declare -r DIVIDER=$(printf "%0.1s" -{1..80})
 declare -r POWERSHELL=$(which pwsh 2>/dev/null || which powershell 2>/dev/null)
+declare -g BUILD_METHOD=powershell
 
 # -----------------------------------------------------------------------------
 # Functions
 # -----------------------------------------------------------------------------
+function usage() {
+  local exit_code=${1:-1}; shift
+  cat <<USAGE
+
+  Usage: ${SCRIPT} <options>
+
+  Options:
+    -h | --help      This message
+    -d | --docker    Build with docker instead of powershell
+
+USAGE
+  exit ${exit_code}
+}
+
+# -----------------------------------------------------------------------------
+
+function parse_options() {
+  while [[ $# -gt 0 ]]; do
+    case ${1} in
+    -d|--docker) BUILD_METHOD=docker;;
+    -h|--help)   usage 0;;
+    *)           usage 1;;
+    esac
+    shift
+  done
+}
+
+# -----------------------------------------------------------------------------
+
 function file_list() {
   find ${BASE_DIR} \
     -type f \
@@ -26,6 +57,27 @@ function file_list() {
     -path "${BASE_DIR}/*Portable/*" | \
     sort
 }
+
+# -----------------------------------------------------------------------------
+
+function build_with_powershell() {
+  script=${1}; shift
+  pwsh \
+    -ExecutionPolicy ByPass \
+    -File ${script} \
+    ${CHECKSUM:--UpdateChecksums}
+}
+
+# -----------------------------------------------------------------------------
+
+function build_with_docker() {
+  name=${1}; shift
+  cd ${BASE_DIR}/${name} && \
+    ${SCRIPT_DIR}/docker-build.sh
+}
+
+# -----------------------------------------------------------------------------
+
 
 # -----------------------------------------------------------------------------
 
@@ -37,7 +89,10 @@ function build_package() {
   echo ${DIVIDER}
   echo Building ${name}
   echo ${DIVIDER}
-  ${POWERSHELL} -ExecutionPolicy ByPass ${script}
+  case ${BUILD_METHOD} in
+  docker) build_with_docker ${name};;
+  *)      build_with_powershell ${script};;
+  esac
   echo
 }
 
@@ -52,4 +107,5 @@ function build_all_packages() {
 # -----------------------------------------------------------------------------
 # Main
 # -----------------------------------------------------------------------------
+parse_options "${@}"
 build_all_packages
