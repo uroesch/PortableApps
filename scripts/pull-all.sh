c6a16088098ab904977f07e198b5416578778068
Urs Roesch <github@bun.ch>
2021-11-20 23:45:23 +0100

    pull-all: Determine default branch

--- scripts/pull-all.sh
+++ scripts/pull-all.sh
@@ -8,11 +8,18 @@ declare -r BASE_DIR=$(readlink --canonicalize $(dirname ${0})/..)
 declare -r TIMESTAMP=$(date +%F)
 declare -r LINE=$(printf "%0.1s" -{1..80})
 
+
+function default_branch() {
+  # prefer master over main
+  local -a branches=( $(git branch | grep -oE "\<(master|main)\>" | sort -r) )
+  echo "${branches[0]}"
+}
+
 function pull_all() {
   local repo_name=${1}; shift;
   cd ${repo_name}
   print_header "${repo_name}"
-  git checkout master
+  git checkout "$(default_branch)"
   git pull --rebase origin master
   for repo in $(git branch | grep -v master); do
     git branch -d ${repo} && git push origin :${repo} || :
