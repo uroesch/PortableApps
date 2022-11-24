a5974fef089c68a95440d941fb85ac36f06f1222
Urs Roesch <github@bun.ch>
2022-11-24 20:35:42 +0100

    docker-build: Add shell switch for debugging

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -11,12 +11,16 @@ set -o pipefail
 # Globals
 # -----------------------------------------------------------------------------
 declare -r SCRIPT=${0##*/}
+declare -r AUTHOR="Urs Roesch"
+declare -r VERSION=0.3.0
+declare -r LICENSE="GPL2"
 declare -r BASE_DIR=$(readlink -f $(dirname ${0})/..)
 declare -r SCRIPT_DIR=$(readlink -f $(dirname $0))
 declare -r UPDATE_SCRIPT=Other/Update/Update.ps1
 declare -a UPDATE_OPTIONS=( -InfraDir /pa-build )
 declare -r DIVIDER=$(printf "%0.1s" -{1..80})
 declare -g DOCKER_IMAGE=uroesch/pa-wine:latest
+declare -g RUN_SHELL=false
 declare -g BUILD_ALL=false
 declare -a BUILD=()
 
@@ -32,10 +36,11 @@ function usage() {
     ${SCRIPT} [<options>]
 
   Options:
-    -h | --help             This message
-    -A | --all              Build all packages
-    -b | --build <PA-name>  Build package with name
-    -u | --up-release       Run in upgrade release mode
+    -A | --all              Build all packages.
+    -b | --build <PA-name>  Build package with name.
+    -h | --help             This message.
+    -s | --shell            Mount everything and run a bash shell.
+    -u | --up-release       Run in upgrade release mode.
 
   Examples:
     Build all PortableApps packages in repository
@@ -60,26 +65,24 @@ USAGE
 function parse_options() {
   while (( ${#} > 0 )); do
     case ${1} in
-    -u|--up-release)
-      UPDATE_OPTIONS+=( -UpdateChecksums )
-      ;;
-    -A|--all)
-      BUILD_ALL=true
-      ;;
-    -b|--build)
-      shift;
-      BUILD_ALL=false
-      BUILD+=( "${1}" )
-      ;;
-    -h|--help)
-      usage 0
-      ;;
-    *)
-      usage 1
-      ;;
+    -A|--all)        BUILD_ALL=true ;;
+    -b|--build)      shift; BUILD_ALL=false; BUILD+=( "${1}" ) ;;
+    -h|--help)       usage 0 ;;
+    -s|--shell)      RUN_SHELL=true ;;
+    -u|--up-release) UPDATE_OPTIONS+=( -UpdateChecksums ) ;;
+    -V|--version)    version ;;
+    *)               usage 1 ;;
     esac
     shift
   done
+}
+
+# -----------------------------------------------------------------------------
+
+function version() {
+  printf "%s v%s\nCopyright (c) %s\nLicense - %s\n" \
+    "${SCRIPT%.*}" "${VERSION}" "${AUTHOR}" "${LICENSE}"
+  exit 0
 }
 
 # -----------------------------------------------------------------------------
@@ -118,17 +121,37 @@ function build_packages() {
 
 # -----------------------------------------------------------------------------
 
-function build_on_docker() {
-  local name=${1}
-  printf "%s\n%s\n%s\n\n" "${DIVIDER}" "Building ${name}" "${DIVIDER}"
+function run_docker() {
+  local name="${1}"; shift
+  local command="${@}";
   docker run \
     --rm \
     --tty \
+    --interactive \
     --env USER_UID=$(id --user) \
     --env USER_GID=$(id --group) \
     --mount type=bind,src=${BASE_DIR},target=/PortableApps \
     --workdir=/PortableApps/${name} \
     ${DOCKER_IMAGE} \
+    ${command}
+}
+
+# -----------------------------------------------------------------------------
+
+function run_shell() {
+  [[ ${RUN_SHELL} != true ]] && return 0
+  local name=$(pwd)
+  run_docker "${name##*/}" /bin/bash 
+  exit
+}
+
+# -----------------------------------------------------------------------------
+
+function build_on_docker() {
+  local name=${1}
+  printf "%s\n%s\n%s\n\n" "${DIVIDER}" "Building ${name}" "${DIVIDER}"
+  run_docker \
+    ${name} \
     pwsh -ExecutionPolicy ByPass ${UPDATE_SCRIPT} ${UPDATE_OPTIONS[@]}
 }
 
@@ -136,4 +159,5 @@ function build_on_docker() {
 # Main
 # -----------------------------------------------------------------------------
 parse_options "${@}"
+run_shell
 build_packages
