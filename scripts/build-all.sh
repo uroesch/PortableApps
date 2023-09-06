2298e1e8a439cb9170cd42bedf810195d0235be4
Urs Roesch <github@bun.ch>
2023-09-06 23:40:48 +0200

    build-all: Add Xvfb for building with powershell
    Add Xvfb when running in the terminal. This ensures
    builds work on a headless server.

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -11,8 +11,11 @@ set -o pipefail
 # Globals
 # -----------------------------------------------------------------------------
 declare -r SCRIPT=${0##*/}
-declare -r BASE_DIR=$(readlink -f $(dirname ${0})/..)
-declare -r SCRIPT_DIR=$(readlink -f $(dirname $0))
+declare -r AUTHOR="Urs Roesch"
+declare -r VERSION="0.2.0"
+declare -r LICENSE="GPL2"
+declare -r BASE_DIR=$(cd $(dirname ${0})/..; pwd)
+declare -r SCRIPT_DIR=$(cd $(dirname $0); pwd)
 declare -r UPDATE_SCRIPT=Update.ps1
 declare -r DIVIDER=$(printf "%0.1s" -{1..80})
 declare -r POWERSHELL=$(which pwsh 2>/dev/null || which powershell 2>/dev/null)
@@ -21,7 +24,7 @@ declare -g BUILD_METHOD=powershell
 # -----------------------------------------------------------------------------
 # Functions
 # -----------------------------------------------------------------------------
-function usage() {
+function ::usage() {
   local exit_code=${1:-1}; shift
   cat <<USAGE
 
@@ -30,6 +33,7 @@ function usage() {
   Options:
     -h | --help      This message
     -d | --docker    Build with docker instead of powershell
+    -V | --version   Print version and exit
 
 USAGE
   exit ${exit_code}
@@ -37,12 +41,13 @@ USAGE
 
 # -----------------------------------------------------------------------------
 
-function parse_options() {
+function ::parse_options() {
   while [[ $# -gt 0 ]]; do
     case ${1} in
-    -d|--docker) BUILD_METHOD=docker;;
-    -h|--help)   usage 0;;
-    *)           usage 1;;
+    -d|--docker)  BUILD_METHOD=docker;;
+    -h|--help)    ::usage 0;;
+    -V|--version) ::version;;
+    *)            ::usage 1;;
     esac
     shift
   done
@@ -50,7 +55,15 @@ function parse_options() {
 
 # -----------------------------------------------------------------------------
 
-function file_list() {
+function ::version() {
+  printf "%s v%s\nCopyright (c) %s\nLicense - %s\n" \
+    "${SCRIPT%.*}" "${VERSION}" "${AUTHOR}" "${LICENSE}"
+  exit 0
+}
+
+# -----------------------------------------------------------------------------
+
+function ::file_list() {
   find ${BASE_DIR} \
     -type f \
     -name ${UPDATE_SCRIPT} \
@@ -59,23 +72,41 @@ function file_list() {
 }
 
 # -----------------------------------------------------------------------------
+# X Functions
+# -----------------------------------------------------------------------------
+function x::create_port() {
+  printf -v DISPLAY ":7%03d" $(( ${RANDOM} % 1000 )) 
+  export DISPLAY
+}
 
-function build_with_powershell() {
+function x::start() {
+  [[ ${XDG_SESSION_TYPE} == tty ]] || return 0 && :
+  x::create_port
+  Xvfb ${DISPLAY} &>/dev/null & 
+}
+
+function x::stop() {
+  pkill -f "Xvfb ${DISPLAY}" &> /dev/null || :
+}
+
+# -----------------------------------------------------------------------------
+# Build Functions
+# -----------------------------------------------------------------------------
+function build::with_powershell() {
   local name=${1}; shift
   local script=${1}; shift
-  echo ${DIVIDER}
-  echo Building ${name}
-  echo ${DIVIDER}
-  echo
+  printf "%s\n" ${DIVIDER} "Building ${name}" ${DIVIDER} ''
+  x::start
   pwsh \
     -ExecutionPolicy ByPass \
     -File ${script} \
     ${CHECKSUM:--UpdateChecksums}
+  x::stop
 }
 
 # -----------------------------------------------------------------------------
 
-function build_with_docker() {
+function build::with_docker() {
   local name=${1}; shift
   cd ${BASE_DIR}/${name} && \
     ${SCRIPT_DIR}/docker-build.sh
@@ -83,30 +114,27 @@ function build_with_docker() {
 
 # -----------------------------------------------------------------------------
 
-
-# -----------------------------------------------------------------------------
-
-function build_package() {
+function build::package() {
   script=${1}; shift;
   name=${script#${BASE_DIR}/}
   name=${name%%/*}
   case ${BUILD_METHOD} in
-  docker) build_with_docker ${name};;
-  *)      build_with_powershell ${name} ${script};;
+  docker) build::with_docker ${name};;
+  *)      build::with_powershell ${name} ${script};;
   esac
   echo
 }
 
 # -----------------------------------------------------------------------------
 
-function build_all_packages() {
-  for script in $(file_list); do
-    build_package "${script}"
+function build::all_packages() {
+  for script in $(::file_list); do
+    build::package "${script}"
   done
 }
 
 # -----------------------------------------------------------------------------
 # Main
 # -----------------------------------------------------------------------------
-parse_options "${@}"
-build_all_packages
+::parse_options "${@}"
+build::all_packages
